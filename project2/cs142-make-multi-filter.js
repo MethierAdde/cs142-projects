8ddffee359d105989a1c07a607675443e69187bd
MethierAdde <2300017783@stu.pku.edu.cn>
2025-02-01 22:22:51 +0800

    project2 problem1 finished

---
+++ project2/cs142-make-multi-filter.js
@@ -0,0 +1,19 @@
+'use script'
+
+function cs142MakeMultiFilter(originalArray){
+    let currentArray = originalArray;
+    function arrayFilter(filterCriteria,callback){
+        if(typeof filterCriteria !== "function"){
+            return originalArray;
+        }
+        currentArray = currentArray.filter(filterCriteria);
+        if(typeof callback === "function")
+        {
+            callback = callback.bind(originalArray);
+            callback(currentArray);
+        }
+        originalArray = currentArray;
+        return arrayFilter;
+    }
+    return arrayFilter;
+}
