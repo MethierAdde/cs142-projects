a579b35fb3ed1406181652350ed85c115825d494
MethierAdde <2300017783@stu.pku.edu.cn>
2025-02-02 20:59:28 +0800

    project2 problem2 finished

---
+++ project2/cs142-template-processor.js
@@ -0,0 +1,20 @@
+'use script';
+
+function Cs142TemplateProcessor(template){
+    this.template = template;
+}
+//这个写法确实厉害，虽说不知道为啥用奇怪的不合题意的写法也能过（）
+Cs142TemplateProcessor.prototype.fillIn = function(dictionary){
+    var re = /{{[0-9a-zA-Z]*}}/g;
+
+    var filledIn = this.template.replace(re, (match) =>{
+        var key = match.slice(2,match.length-2);
+        var word = dictionary[key];
+        if(word === undefined)
+            return "";
+        else 
+            return word;
+    });
+
+    return filledIn;
+}
